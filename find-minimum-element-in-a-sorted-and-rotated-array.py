c93b76774b0468d962c81abe3fa5102813183a58
ishankfl <ishankafle5@gmail.com>
2025-02-02 09:34:22 +0545

    7-find-minimum-element-in-a-sorted-and-rotated-array

---
+++ find-minimum-element-in-a-sorted-and-rotated-array.py
@@ -0,0 +1,9 @@
+
+def findMin(array):
+    res = array[0]
+    for i in range(len(array)):
+        res = min(res, array[i])
+    return res
+
+if __name__ == "__main__":
+    print(findMin([1,2,3,4,5,6,2,3, -100]))
